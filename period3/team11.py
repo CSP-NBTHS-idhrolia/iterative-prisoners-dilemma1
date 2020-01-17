c684d835268d65b3b65cee5559200b6a732a21c3
iMuda <59832435+iMuda@users.noreply.github.com>
2020-01-17 09:20:28 -0500

    Update team11.py

--- period3/team11.py
+++ period3/team11.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = '' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
@@ -17,6 +17,18 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+import random
+if len(my_history)==0:
+    return 'b'
+else:
+    if 'c' in their_history[-1]:
+        return 'b'
+    else:
+        if their_score >-200
+            return 'c'
+        else:
+            return 'b'
+
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
