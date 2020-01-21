5fbd899c4a3b395582648a192b7b6e9f2674b0cb
Albert Lewis <45643396+BertieMcBergensen@users.noreply.github.com>
2020-01-21 07:52:03 -0500

    Added Code
    Added code for my move

--- period3/team1.py
+++ period3/team1.py
@@ -16,13 +16,11 @@ def move(my_history, their_history, my_score, their_score):
     goodboy_points = 0
 
     if len(my_history) == 0:
-        print('b')
-        return 'b'
-
+        return 'c'
+    
     if their_score > my_score:
-        print('b')
         return 'b'
-  
+    
     for i in their_history:
         if i == 'b':
             b_c_value += -10
@@ -30,17 +28,11 @@ def move(my_history, their_history, my_score, their_score):
             b_c_value += +10
 
     goodboy_points = b_c_value / len(their_history)
-    print("b_c_value = " + str(b_c_value))
-    print("good boi points = " + str(goodboy_points))
 
-    if random.randint(0, 10) <= goodboy_points:
-            print('c')
+    if random.randint(-10, 10) <= goodboy_points:
             return 'c'
     else:
-            print('b')
             return 'b'
-
-    print('doneall')
 
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -50,7 +42,7 @@ def move(my_history, their_history, my_score, their_score):
     '''
 
     
-def test_move(my_history, their_history, my_score, their_score, result):
+def test_move(my_history, their_history, my_score, their_score, result = random.choice(['a','b'])):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
     Returns True or False, dpending on whether result was as expected.
@@ -58,29 +50,29 @@ def test_move(my_history, their_history, my_score, their_score, result):
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:
         return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+") returned " + "'" + real_result + "'" + " and should have returned '" + result + "'")
-        return False
+    
+    return False
 
 if __name__ == '__main__':
      
-    # Test 1: Betray on first move.
+    # Test 1: Collude on first move.
     if test_move(my_history='',
               their_history='', 
               my_score=0,
               their_score=0,
-              result='b'):
+              result='c'):
          print('Test1: passed')
     
     # Test 2: They have a high goodie points
-    if test_move(my_history='bbbbbbbb',
-              their_history='ccccccbb', 
+    if test_move(my_history='bbbbbbbbbbbbbb',
+              their_history='ccccccccccccbb', 
               my_score = 0,
               their_score = 0,
               result='c'):
          print('Test2: passed')
+    else:
+        print("Test2: failed")
+    
     
     # Test 3: They have high goodie points but higher score
     if test_move(my_history='bbbbbbbb',
@@ -89,6 +81,9 @@ if __name__ == '__main__':
               their_score = 100,
               result='b'):
          print('Test3: passed')
+    else:
+        print('Test3: Failed')
+
     # Test 4: They have low goodie points
     if test_move(my_history='bbbbbbbb',
               their_history='bbbbbbcc', 
@@ -96,6 +91,19 @@ if __name__ == '__main__':
               their_score = 0,
               result= 'b'):
          print('Test4: passed')
+    else:
+        print('test4: failed')
+
+    # Test 5: Medium
+    if test_move(my_history='bbbbbbbb',
+              their_history='bbbbcccc', 
+              my_score = 0,
+              their_score = 0,
+              result= 'b'):
+         print('Test5: passed')
+    else:
+        print('Test5: failed')
+    
     
     
 
