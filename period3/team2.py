3d553e286d0a4d56780a0f1e7f92b68548fd5478
ashnasinha <ashna.sinha@gmail.com>
2020-01-21 08:02:39 -0500

    Update team2.py

--- period3/team2.py
+++ period3/team2.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'kinda but ok' # Only 10 chars displayed.
+team_name = 'kinda bad but ok' # Only 10 chars displayed.
 strategy_name = 'Betray as much as possible.'
 strategy_description = 'Collude, and then betray everything else.'
     
